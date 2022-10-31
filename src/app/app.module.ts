047aebc9bdbcb8e308317ae4014a71e4a794d3eb
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-10-30 23:41:52 -0300

    4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OlaMundoComponent
   ],
   imports: [
     BrowserModule
